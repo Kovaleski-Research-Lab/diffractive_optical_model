b6ab6b98ee186279835f62d53027373220ecec67
Marshall Lindsay <mblgh6@umsystem.edu>
2024-10-04 11:50:49 -0500

    Removed python from requirements. Updated setup.py. Added manifest to include the config file

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
+# Utility function to read the requirements.txt file
 def read_requirements():
     with open('requirements.txt') as req:
-        content = req.read()
-        requirements = content.split('\n')
-    return list(requirements)
+        return req.read().splitlines()
 
 setup(
     name='diffractive_optical_model',
@@ -13,6 +12,11 @@ setup(
     author='Marshall B. Lindsay',
     author_email='mblgh6@umsystem.edu',
     packages=find_packages(),
-    install_requires=read_requirements(),
+    install_requires=read_requirements(),  # Read from requirements.txt
+    include_package_data=True,  # This ensures non-code files are included
+    package_data={
+        # Specify any additional files to include in the package, such as data files, YAML configs, etc.
+        '': ['config.yaml', 'data/**/*.bmp', 'data/**/*.pgm', 'data/**/*.ppm']
+    }
 )
 
